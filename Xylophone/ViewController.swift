0644a9eb5fc8e3e9ee353981d7f8cf767cbcb06d
deshollow <555demin@gmail.com>
2024-02-14 13:08:11 +0300

    dispatchqueue + alpha / done

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -12,22 +12,27 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
         
         playSound(currentTitle: sender.currentTitle ?? "C")
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(currentTitle: String) {
         let url = Bundle.main.url(forResource: currentTitle, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
+        
     }
 }
 
